91fae32169ffc93f4071bbb3bc2469baab75b3b8
SantiRC05 <santycar82@gmail.com>
2026-03-17 23:10:22 -0600

    Interfaces

--- typescript-intro/src/main.ts
+++ typescript-intro/src/main.ts
@@ -2,6 +2,8 @@ import './style.css';
 
 //import './topics/01-basic-types'
 
+import './topics/02-objetc-interface;
+
 const app = document.querySelector<HTMLDivElement>('#app')!;
 
 app.innerHTML = 'Hola Mundooooo'
--- typescript-intro/src/topics/02-objetc-interface.ts
+++ typescript-intro/src/topics/02-objetc-interface.ts
@@ -1,9 +1,21 @@
 const skills: string[] = ['Bash', 'Counter', 'Healing']
 
 
-const strider = {
+interface Character {
+    name: string;
+    hp: number;
+    skills: string[];
+    hometown?: string;
+}
+
+const strider: Character = {
     name: 'Strider',
     hp: 100,
     skills: ['Bash', 'Counter']
 }
 
+strider.hometown = 'Rivendell';
+
+console.table(strider)
+
+export{}

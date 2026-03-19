f0b532a2d5727667daa2044e2b6ffb5cf889d932
SantiRC05 <santycar82@gmail.com>
2026-03-18 21:54:53 -0600

    Character and Hp

--- src/topics/03-functions.ts
+++ src/topics/03-functions.ts
@@ -15,10 +15,30 @@ const result:number = addNumbers(1, 2)
 const resultArrow:string = addNumbersArrow(1, 2)
 const resultMultiply:number = multiply(5)
 
-console.log({result, resultArrow, resultMultiply})
+//console.log({result, resultArrow, resultMultiply})
 
 
-console.log({result, resultArrow, resultMultiply})
+interface Character {
+    name: string;
+    hp: number;
+    showHp: () => void
+}
+
+const healCharacter = (character: Character, amount: number) => {
+    character.hp += amount;
+}
+
+const strider: Character = {
+    name: 'Strider',
+    hp: 50,
+    showHp() {
+        console.log(`Puntos de vida: ${this.hp} - 03-functions.ts:35`);
+        }
+}
+
+healCharacter(strider, 30);
+
+strider.showHp();
 
 
 

00810bca32b545cdfaf07699f9f29559aee4b95c
SantiRC05 <santycar82@gmail.com>
2026-03-21 18:15:50 -0600

    Clases

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,8 @@ import './style.css';
 
 import './topics/04-homework-types'
 
+import './topics/08-Clases'
+
 const app = document.querySelector<HTMLDivElement>('#app')!;
 
 app.innerHTML = 'Hola Mundooooo'
---
+++ src/topics/08-Clases.ts
@@ -0,0 +1,15 @@
+
+export class Person {
+    public name: string;
+    public address: string;
+
+    constructor() {
+        this.name = 'Santi';
+        this.address = 'Coronado'
+    }
+}  
+
+const ironman = new Person();
+
+
+console.log(ironman);

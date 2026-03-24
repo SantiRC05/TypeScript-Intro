927534234fd58774fd81f24dae7aa60fbb098d62
SantiRC05 <santycar82@gmail.com>
2026-03-23 22:10:41 -0600

    herencias

--- src/topics/08-Clases.ts
+++ src/topics/08-Clases.ts
@@ -1,3 +1,4 @@
+import { Person } from './08-Clases';
 
 export class Person {
     //public name: string;
@@ -9,17 +10,37 @@ export class Person {
     ) {}
 }  
 
+//export class Hero extends Person {
+    //constructor(
+        //public alterEgo: string,
+        //public age: number,
+       // public realName: string
+    //) {
+        //super(realName, 'New York');
+    //}
+//}
+
 export class Hero extends Person {
+    public get realName(): string {
+        return this._realName;
+    }
+    public set realName(value: string) {
+        this._realName = value;
+    }
+    public person: Person;
+
     constructor(
         public alterEgo: string,
         public age: number,
-        public realName: string
+        private _realName: string,
+        public person: Person
     ) {
-        super(realName, 'New York');
+
     }
 }
 
-const ironman = new Hero('Ironman', 45, 'Tony Stark');
+const tony = new Person('Tony Stark', 'New York');
+const ironman = new Hero('Ironman', 45, 'Tony Stark', tony);
 
 
 console.log(ironman);

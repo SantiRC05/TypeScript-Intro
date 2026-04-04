12cdfb81bc1d528b3c71f3a11e3de6564ec70503
SantiRC05 <santycar82@gmail.com>
2026-04-03 21:04:44 -0600

    conflictos

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import './style.css';
 
+
+
+
 // module declaration moved to a global declaration file
 
+
+
+
 //import './topics/01-basic-types'
+
+
+
+
+
 
 import './topics/02-objetc-interface';
 
